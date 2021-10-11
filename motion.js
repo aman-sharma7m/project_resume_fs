b0b678cf9d1d4a4b2dfbdc1eaeb17cb6684ea842
aman-sharma7m <aman.7m@outlook.com>
2021-10-11 22:22:14 +0530

    added the fillup at skill

--- motion.js
+++ motion.js
@@ -28,3 +28,52 @@ function scrollVertical(targetSectionId){
   window.scrollBy(0,10);
 }
   
+// skill bar changes begin
+var animationDone=false;
+var skillContainer=document.querySelector('.skills-display > div');
+var progressBars=document.querySelectorAll('.skill-progress div');
+
+function initializeZeroWidth(){
+  for (let bar of progressBars){
+    bar.style.width=0+'%';
+  }
+}
+
+var fillValue=new Array();
+for(let i=0;i<progressBars.length;i++){
+  fillValue[i]=progressBars[i].clientWidth/150*100;
+}
+
+function fillBar(){
+  for (let i=0;i<progressBars.length;i++){
+    var targetWidth=fillValue[i];
+    var currentWidth=0;
+    var interval=setInterval(function(){
+      if (currentWidth>targetWidth){
+        clearInterval(interval);
+        return;
+      }
+      currentWidth++;
+      progressBars[i].style.width=currentWidth+'%';
+    },30);
+  }
+}
+window.addEventListener('scroll',checkScroll);
+
+function checkScroll(){
+  //have to check skill container is visible
+  var coordinate=skillContainer.getBoundingClientRect();
+  
+  if (coordinate.top< window.innerHeight && !animationDone){
+    //to stop multiple fires
+    animationDone=true;
+    
+    //initialise width to zero
+    initializeZeroWidth();
+    //fire the animation
+    fillBar();
+
+      
+  }
+}
+// skill bar changes end 
