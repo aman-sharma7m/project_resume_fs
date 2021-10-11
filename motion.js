659a041f1cc1609d08c91bdb975dc4ee60f7d310
aman-sharma7m <aman.7m@outlook.com>
2021-10-11 19:24:51 +0530

    added another vertical scroll approach

--- motion.js
+++ motion.js
@@ -1,6 +1,8 @@
 
 var navButton=document.querySelectorAll('.nav-menu a');
 // console.log(navButton)
+var interval;
+
 for (var i=0;i<navButton.length;i++){
   navButton[i].addEventListener('click',function(event){
     event.preventDefault();
@@ -11,18 +13,18 @@ for (var i=0;i<navButton.length;i++){
     
     //console.log(targetSectionCoordinate['y']);
 
-    var interval=setInterval(function(){
-      var targetSectionCoordinate=targetSectionId.getBoundingClientRect();
-      console.log(targetSectionCoordinate.top);
-      if (targetSectionCoordinate.top<=0){
-        clearInterval(interval);
-        return;
-      }
-      window.scrollBy(0,10);
-    },10);
+    interval=setInterval(scrollVertical,10,targetSectionId);
 
   });
 }
 
-
+function scrollVertical(targetSectionId){
+  var targetSectionCoordinate=targetSectionId.getBoundingClientRect();
+  console.log(targetSectionCoordinate.top);
+  if (targetSectionCoordinate.top<=0){
+    clearInterval(interval);
+    return;
+  }
+  window.scrollBy(0,10);
+}
   
